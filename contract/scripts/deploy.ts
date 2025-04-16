7635a96bfc3aa3c2628c19da99ee8db3cac6d34c
Insu-Lee <insu.lee@mrhi.or.kr>
2025-04-16 13:14:42 +0900

    fix: deploy script

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -7,17 +7,6 @@ async function main() {
   console.log(`Deploying contracts with the account: ${deployer.address}`);
 
   // Todo: 배포하기 위한 script를 만들어주세요.
-  // 배포할 recipient 주소를 첫 번째 계정으로 설정
-  const recipient = deployer.address;
-
-  // DataType 컨트랙트 팩토리 가져오기
-  const DataType = await ethers.getContractFactory('DataType');
-
-  // 컨트랙트 배포
-  const contract = await DataType.deploy(recipient);
-
-  // 배포 완료 대기
-  await contract.waitForDeployment();
 
   console.log(`DataType contract deployed at: ${contract.target}`);
 
